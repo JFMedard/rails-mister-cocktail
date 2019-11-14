357e9c5f907b511302bfa382d754f752327c5674
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-11-14 16:10:19 +0100

    controllers and views ok

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,36 @@
 class DosesController < ApplicationController
 
-  # def destroy
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @cocktail = Cocktail.new
+    @dose = Dose.new
+  end
 
-  # end
+  def create
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @dose = Dose.new(doses_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(@dose[:ingredient_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
 end
+
+
