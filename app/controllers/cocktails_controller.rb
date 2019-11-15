2410e690f805055da9d37467be7d42bcb2070a9d
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-11-15 13:55:09 +0100

    duuh

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -28,7 +28,7 @@ class CocktailsController < ApplicationController
 
   def update
     @cocktail = Cocktail.find(params[:id])
-    @cocktail.update
+    @cocktail.update(cocktail_params)
     redirect_to cocktail_path(@cocktail)
   end
 
